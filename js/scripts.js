2d4c9211fe82ae064b25d7d84ff807e32b9c54bb
lyndoncore <lyndoncore@gmail.com>
2019-02-21 20:21:38 -0600

    add gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-$(document).ready(function(){
-  $(".hamburger").click(function(){
-    $(this).toggleClass("is-active");
-  });
-});
+window.onload=function() {
+  var button = document.getElementById("hamburger-button");
+  var menu = document.getElementById("hamburger-menu");
+
+  button.onclick=function() {
+	  if (menu.classList.contains('m-menu--hidden')) {
+	    menu.classList.add('m-menu');
+	    menu.classList.remove('m-menu--hidden');
+	  } else {
+	  	menu.classList.add('m-menu--hidden');
+	   	menu.classList.remove('m-menu');
+	  }
+	}
+}
